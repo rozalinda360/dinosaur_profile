1f5eecb17d9e094baa7143d0affa3085882a3d1d
rozalinda360 <razhus1999@gmail.com>
2023-07-03 07:18:43 +0400

    coin

--- CactiController.js
+++ CactiController.js
@@ -1,71 +1,189 @@
 import Cactus from "./Cactus.js";
+import Coin from "./Coin.js";
 
 export default class CactiController {
-    
-    CACTUS_INTERVAL_MIN = 2000;
-    CACTUS_INTERVAL_MAX = 3000;
 
-    nextCactusInterval = null;
-    cacti = [];
+  CACTUS_INTERVAL_MIN = 500;
+  CACTUS_INTERVAL_MAX = 2000;
 
-    constructor(ctx, cactiImages, scaleRatio, speed) {
-        this.ctx = ctx;
-        this.canvas = ctx.canvas;
-        this.cactiImages = cactiImages;
-        this.scaleRatio = scaleRatio;
-        this.speed = speed;
+  COIN_INTERVAL_MIN = 2000;
+  COIN_INTERVAL_MAX = 5000;
 
-        this.setNextCactusTime();
+  nextCactusInterval = null;
+  nextCoinInterval = null;
+  cacti = [];
+  coins = [];
+
+  constructor(ctx, cactiImages, scaleRatio, speed) {
+    this.ctx = ctx;
+    this.canvas = ctx.canvas;
+    this.cactiImages = cactiImages;
+    this.scaleRatio = scaleRatio;
+    this.speed = speed;
+
+    this.setNextCactusTime();
+    this.setNextCoinTime();
+  }
+
+  setNextCactusTime() {
+    const num = this.getRandomNumber(
+      this.CACTUS_INTERVAL_MIN,
+      this.CACTUS_INTERVAL_MAX
+    );
+
+    this.nextCactusInterval = num;
+  }
+
+  setNextCoinTime() {
+    const num = this.getRandomNumber(
+      this.COIN_INTERVAL_MIN,
+      this.COIN_INTERVAL_MAX
+    );
+
+    this.nextCoinInterval = num;
+  }
+
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  createCactus() {
+    const index = this.getRandomNumber(0, this.cactiImages.length - 1);
+    const cactusImage = this.cactiImages[index];
+    const x = this.canvas.width * 1.5;
+    const y = this.canvas.height - cactusImage.height;
+    const cactus = new Cactus(
+      this.ctx,
+      x,
+      y,
+      cactusImage.width,
+      cactusImage.height,
+      cactusImage.image
+    );
+
+    this.cacti.push(cactus);
+  }
+
+  createCoin() {
+    const x = this.canvas.width * 1.5;
+    const y = 80;
+    const coin = new Coin(this.ctx, x, y, this.speed, this.scaleRatio);
+    this.coins.push(coin);
+  }
+
+  update(gameSpeed, frameTimeDelta) {
+    if (this.nextCactusInterval <= 0) {
+      this.createCactus();
+      this.setNextCactusTime();
     }
+    this.nextCactusInterval -= frameTimeDelta;
 
-    setNextCactusTime() {
-        const num = this.getRandomNumber(
-            this.CACTUS_INTERVAL_MIN,
-            this.CACTUS_INTERVAL_MAX
-        );
-        
-        this.nextCactusInterval = num;
+    if (this.nextCoinInterval <= 0) {
+      this.createCoin();
+      this.setNextCoinTime();
     }
+    this.nextCoinInterval -= frameTimeDelta;
 
-    getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+    this.cacti.forEach((cactus) => {
+      cactus.update(this.speed, gameSpeed, frameTimeDelta, this.scaleRatio);
+    });
 
+    this.coins.forEach((coin) => {
+      coin.update(this.speed, gameSpeed, frameTimeDelta, this.scaleRatio);
+    });
 
-    createCactus() {
-        const index = this.getRandomNumber(0, this.cactiImages.length - 1);
-        const cactusImage = this.cactiImages[index];
-        const x = this.canvas.width * 1.5;
-        const y = this.canvas.height - cactusImage.height;
-        const cactus = new Cactus(this.ctx, x, y, cactusImage.width, cactusImage.height, cactusImage.image);
-        this.cacti.push(cactus);
-    
-    }
+    this.cacti = this.cacti.filter((cactus) => cactus.x > -cactus.width);
+    // this.coins = this.coins.filter((coin) => coin.x > -coin.width);
+  }
 
-    update(gameSpeed, frameTimeDelta) {
-        if (this.nextCactusInterval <= 0) {
-            this.createCactus();
-            this.setNextCactusTime();
-        }
-        this.nextCactusInterval -= frameTimeDelta;
-        console.log(this.cacti.length);
+  draw() {
+    this.cacti.forEach((cactus) => cactus.draw());
+    console.log(this.coins.length);
+    this.coins.forEach((coin) => coin.draw());
+  }
 
-        this.cacti.forEach((cactus)=>{
-            cactus.update(this.speed,gameSpeed, frameTimeDelta,this.scaleRatio);
-        });
-        this.cacti = this.cacti.filter((cactus)=> cactus.x > -cactus.width);
-    }
+  collideWith(sprite) {
+    return (
+      this.cacti.some((cactus) => cactus.collideWith(sprite))
+      ||
+      this.coins.some((coin) => coin.collideWith(sprite))
+    );
+  }
 
-    draw() {
-        this.cacti.forEach((cactus) => cactus.draw());
-    }
+  reset() {
+    this.cacti = [];
+    this.coins = [];
+  }
 
-    collideWith(sprite){
-        return this.cacti.some((cactus) => cactus.collideWith(sprite));
-    }
+  // CACTUS_INTERVAL_MIN = 500;
+  // CACTUS_INTERVAL_MAX = 2000;
 
+  // nextCactusInterval = null;
+  // cacti = [];
 
-    reset(){
-        this.cacti = [];
-    }
+  // constructor(ctx, cactiImages, scaleRatio, speed) {
+  //   this.ctx = ctx;
+  //   this.canvas = ctx.canvas;
+  //   this.cactiImages = cactiImages;
+  //   this.scaleRatio = scaleRatio;
+  //   this.speed = speed;
+
+  //   this.setNextCactusTime();
+  // }
+
+  // setNextCactusTime() {
+  //   const num = this.getRandomNumber(
+  //     this.CACTUS_INTERVAL_MIN,
+  //     this.CACTUS_INTERVAL_MAX
+  //   );
+
+  //   this.nextCactusInterval = num;
+  // }
+
+  // getRandomNumber(min, max) {
+  //   return Math.floor(Math.random() * (max - min + 1) + min);
+  // }
+
+  // createCactus() {
+  //   const index = this.getRandomNumber(0, this.cactiImages.length - 1);
+  //   const cactusImage = this.cactiImages[index];
+  //   const x = this.canvas.width * 1.5;
+  //   const y = this.canvas.height - cactusImage.height;
+  //   const cactus = new Cactus(
+  //     this.ctx,
+  //     x,
+  //     y,
+  //     cactusImage.width,
+  //     cactusImage.height,
+  //     cactusImage.image
+  //   );
+
+  //   this.cacti.push(cactus);
+  // }
+
+  // update(gameSpeed, frameTimeDelta) {
+  //   if (this.nextCactusInterval <= 0) {
+  //     this.createCactus();
+  //     this.setNextCactusTime();
+  //   }
+  //   this.nextCactusInterval -= frameTimeDelta;
+
+  //   this.cacti.forEach((cactus) => {
+  //     cactus.update(this.speed, gameSpeed, frameTimeDelta, this.scaleRatio);
+  //   });
+
+  //   this.cacti = this.cacti.filter((cactus) => cactus.x > -cactus.width);
+  // }
+
+  // draw() {
+  //   this.cacti.forEach((cactus) => cactus.draw());
+  // }
+
+  // collideWith(sprite) {
+  //   return this.cacti.some((cactus) => cactus.collideWith(sprite));
+  // }
+
+  // reset() {
+  //   this.cacti = [];
+  // }
 }
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import Player from "./player.js";
-import Ground from "./ground.js";
+import Player from "./Player.js";
+import Ground from "./Ground.js";
 import CactiController from "./CactiController.js";
 import Score from "./Score.js";
-import CoinController from "./CoinController.js";
+import Coin from "./Coin.js";
 
-const canvas = document.getElementById('game');
-let ctx = canvas.getContext("2d");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
-const GAME_SPEED_START = 0.75;
+const GAME_SPEED_START = 1; // 1.0
 const GAME_SPEED_INCREMENT = 0.00001;
 
 const GAME_WIDTH = 800;
@@ -24,16 +24,15 @@ const GROUND_HEIGHT = 24;
 const GROUND_AND_CACTUS_SPEED = 0.5;
 
 const CACTI_CONFIG = [
-    {width:48/1.5, height:100/1.5, image:"img/cactus_1.png"},
-    {width:98/1.5, height:100/1.5, image:"img/cactus_2.png"},
-    {width:68/1.5, height:70/1.5, image:"img/cactus_3.png"},
+  { width: 48 / 1.5, height: 100 / 1.5, image: "images/cactus_1.png" },
+  { width: 98 / 1.5, height: 100 / 1.5, image: "images/cactus_2.png" },
+  { width: 68 / 1.5, height: 70 / 1.5, image: "images/cactus_3.png" },
 ];
 
 //Game Objects
 let player = null;
 let ground = null;
 let cactiController = null;
-let coinController = null;
 let score = null;
 
 let scaleRatio = null;
@@ -43,162 +42,181 @@ let gameOver = false;
 let hasAddedEventListenersForRestart = false;
 let waitingToStart = true;
 
-function showGameOver(){
-    const fontSize = 50*scaleRatio;
-    ctx.font = `${fontSize}px GameFont`;
-    ctx.fillStyle = "grey";
-    const x = canvas.width / 4.5;
-    const y = canvas.height / 2;
-    ctx.fillText("GAME OVER",x,y);
-}
-
-function setupGameReset(){
-    if(!hasAddedEventListenersForRestart){
-        hasAddedEventListenersForRestart = true;
-
-        setTimeout(()=>{
-            window.addEventListener("keyup",reset,{once:true});
-            window.addEventListener("touchstart",reset,{once:true});
-        }, 1000);
-    }
-}
-
-function reset(){
-    hasAddedEventListenersForRestart = false;
-    gameOver = false;
-    waitingToStart = false;
-    ground.reset();
-    cactiController.reset();
-    coinController.reset();
-    score.reset();
-    gameSpeed = GAME_SPEED_START;
-
-}
-
-function showStartGame(){
-    const fontSize = 20*scaleRatio;
-    ctx.font = `${fontSize}px GameFont`;
-    ctx.fillStyle = "grey";
-    const x = canvas.width / 14;
-    const y = canvas.height / 2;
-    ctx.fillText("Tap Screen or Press Space to Start",x,y);
-
-}
-
-function updateGameSpeed(frameTimeDelta){
-    gameSpeed += frameTimeDelta * GAME_SPEED_INCREMENT;
-}
-
 function createSprites() {
-    const playerWidthInGame = PLAYER_WIDTH * scaleRatio;
-    const PlayerHeightInGame = PLAYER_HEIGHT * scaleRatio;
-    const minJumpHeightInGame = MIN_JUMP_HEIGHT * scaleRatio;
-    const maxJumpHeightInGame = MAX_JUMP_HEIGHT * scaleRatio;
+  const playerWidthInGame = PLAYER_WIDTH * scaleRatio;
+  const playerHeightInGame = PLAYER_HEIGHT * scaleRatio;
 
-    const groundWidthInGame = GROUND_WIDTH*scaleRatio;
-    const groundHeightInGame = GROUND_HEIGHT*scaleRatio;
+  const minJumpHeightInGame = MIN_JUMP_HEIGHT * scaleRatio;
+  const maxJumpHeightInGame = MAX_JUMP_HEIGHT * scaleRatio;
 
-    ground = new Ground(ctx,groundWidthInGame,groundHeightInGame,GROUND_AND_CACTUS_SPEED,scaleRatio);
-    player = new Player(ctx, playerWidthInGame, PlayerHeightInGame, minJumpHeightInGame, maxJumpHeightInGame, scaleRatio);
+  const groundWidthInGame = GROUND_WIDTH * scaleRatio;
+  const groundHeightInGame = GROUND_HEIGHT * scaleRatio;
 
-    const cactiImages = CACTI_CONFIG.map(cactus =>{
+  player = new Player(
+    ctx,
+    playerWidthInGame,
+    playerHeightInGame,
+    minJumpHeightInGame,
+    maxJumpHeightInGame,
+    scaleRatio
+  );
+
+  ground = new Ground(
+    ctx,
+    groundWidthInGame,
+    groundHeightInGame,
+    GROUND_AND_CACTUS_SPEED,
+    scaleRatio
+  );
+
+
+  const cactiImages = CACTI_CONFIG.map((cactus) => {
     const image = new Image();
     image.src = cactus.image;
     return {
-        image:image,
-        width: cactus.width*scaleRatio,
-        height: cactus.height*scaleRatio,
+      image: image,
+      width: cactus.width * scaleRatio,
+      height: cactus.height * scaleRatio,
     };
+  });
 
-    });
+  cactiController = new CactiController(
+    ctx,
+    cactiImages,
+    scaleRatio,
+    GROUND_AND_CACTUS_SPEED
+  );
 
-    cactiController = new CactiController(ctx,cactiImages,scaleRatio,GROUND_AND_CACTUS_SPEED);
-    coinController = new CoinController(ctx,scaleRatio,GROUND_AND_CACTUS_SPEED);
-    score = new Score(ctx,scaleRatio);
-}
-
-
-function getScaleRatio() {
-    const screenHeight = Math.min(window.innerHeight, document.documentElement.clientHeight);
-    const screenWidth = Math.min(window.innerWidth, document.documentElement.clientWidth);
-    //Window is wider than game width
-    if (screenWidth / screenHeight < GAME_WIDTH / GAME_HEIGHT) {
-        return screenWidth / GAME_WIDTH;
-    }
-    //Window is narrower than game width
-    else {
-        return screenHeight / GAME_HEIGHT;
-    }
+  score = new Score(ctx, scaleRatio);
 }
 
 function setScreen() {
-    scaleRatio = getScaleRatio();
-    canvas.width = GAME_WIDTH * scaleRatio;
-    canvas.height = GAME_HEIGHT * scaleRatio;
-    createSprites();
+  scaleRatio = getScaleRatio();
+  canvas.width = GAME_WIDTH * scaleRatio;
+  canvas.height = GAME_HEIGHT * scaleRatio;
+  createSprites();
+}
 
+setScreen();
+//Use setTimeout on Safari mobile rotation otherwise works fine on desktop
+window.addEventListener("resize", () => setTimeout(setScreen, 500));
+
+if (screen.orientation) {
+  screen.orientation.addEventListener("change", setScreen);
+}
+
+function getScaleRatio() {
+  const screenHeight = Math.min(
+    window.innerHeight,
+    document.documentElement.clientHeight
+  );
+
+  const screenWidth = Math.min(
+    window.innerWidth,
+    document.documentElement.clientWidth
+  );
+
+  //window is wider than the game width
+  if (screenWidth / screenHeight < GAME_WIDTH / GAME_HEIGHT) {
+    return screenWidth / GAME_WIDTH;
+  } else {
+    return screenHeight / GAME_HEIGHT;
+  }
+}
+
+function showGameOver() {
+  const fontSize = 70 * scaleRatio;
+  ctx.font = `${fontSize}px Verdana`;
+  ctx.fillStyle = "grey";
+  const x = canvas.width / 4.5;
+  const y = canvas.height / 2;
+  ctx.fillText("GAME OVER", x, y);
+}
+
+function setupGameReset() {
+  if (!hasAddedEventListenersForRestart) {
+    hasAddedEventListenersForRestart = true;
+
+    setTimeout(() => {
+      window.addEventListener("keyup", reset, { once: true });
+      window.addEventListener("touchstart", reset, { once: true });
+    }, 1000);
+  }
+}
+
+function reset() {
+  hasAddedEventListenersForRestart = false;
+  gameOver = false;
+  waitingToStart = false;
+  ground.reset();
+  cactiController.reset();
+  coin.reset();
+  score.reset();
+  gameSpeed = GAME_SPEED_START;
+}
+
+function showStartGameText() {
+  const fontSize = 40 * scaleRatio;
+  ctx.font = `${fontSize}px Verdana`;
+  ctx.fillStyle = "grey";
+  const x = canvas.width / 14;
+  const y = canvas.height / 2;
+  ctx.fillText("Tap Screen or Press Space To Start", x, y);
+}
+
+function updateGameSpeed(frameTimeDelta) {
+  gameSpeed += frameTimeDelta * GAME_SPEED_INCREMENT;
 }
 
 function clearScreen() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function gameLoop(currentTime) {
-    if (previousTime === null) {
-        previousTime = currentTime;
-        requestAnimationFrame(gameLoop);
-        return;
-    }
-
-    const frameTimeDelta = currentTime - previousTime;
+  if (previousTime === null) {
     previousTime = currentTime;
-    clearScreen();
+    requestAnimationFrame(gameLoop);
+    return;
+  }
+  const frameTimeDelta = currentTime - previousTime;
+  previousTime = currentTime;
 
-    if(!gameOver && !waitingToStart){
-    //update
+  clearScreen();
+
+  if (!gameOver && !waitingToStart) {
+    //Update game objects
     ground.update(gameSpeed, frameTimeDelta);
-    cactiController.update(gameSpeed,frameTimeDelta);
-    coinController.update(gameSpeed,frameTimeDelta);
+    cactiController.update(gameSpeed, frameTimeDelta);
     player.update(gameSpeed, frameTimeDelta);
     score.update(frameTimeDelta);
     updateGameSpeed(frameTimeDelta);
-    }
+  }
 
-    if(!gameOver && cactiController.collideWith(player)){
-        gameOver = true;
-        setupGameReset();
-        score.setHighScore();
-    }
-    //draw
-    ground.draw();
-    cactiController.draw();
-    coinController.draw();
-    player.draw();
-    score.draw();
+  if (!gameOver && cactiController.collideWith(player)) {
+    gameOver = true;
+    setupGameReset();
+    score.setHighScore();
+  }
 
-    if(gameOver){
-        showGameOver();
-    }
+  //Draw game objects
+  ground.draw();
+  cactiController.draw();
+  player.draw();
+  score.draw();
 
-    if(waitingToStart){
-        showStartGame();
-    }
-    requestAnimationFrame(gameLoop);
-}
+  if (gameOver) {
+    showGameOver();
+  }
 
+  if (waitingToStart) {
+    showStartGameText();
+  }
 
-
-
-setScreen();
-window.addEventListener('resize', () => setTimeout(setScreen, 500));
-
-if (screen.orientation) {
-    screen.orientation.addEventListener('change', setScreen);
+  requestAnimationFrame(gameLoop);
 }
 
 requestAnimationFrame(gameLoop);
 
-window.addEventListener("keyup",reset,{once:true});
-window.addEventListener("touchstart",reset,{once:true});
+window.addEventListener("keyup", reset, { once: true });
+window.addEventListener("touchstart", reset, { once: true });
